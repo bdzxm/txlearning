b14fe2636f6918b54acb64af61a970bbd4a39f63
zhaoxumeng <xumeng.zhao@luckincoffee.com>
2020-08-02 10:50:24 +0800

    测试工程可用

---
+++ src/main/java/Helloword.java
@@ -0,0 +1,11 @@
+/**
+ * @program: com.tx.learning3
+ * @description:
+ * @author: xumeng.zhao
+ * @create: 2020-08-02 10:48
+ */
+public class Helloword {
+    public static void main(String[] args) {
+        System.out.println("HelloWord");
+    }
+}
